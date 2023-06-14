770c9e05749327eeb16b2ab64a994df2db75754f
otavio <otavioagusutomperes2703@gmail.com>
2023-06-14 16:54:54 -0300

    change of style and more content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import './styles/types.css'
 import Image from "next/image";
 import Link from "next/link";
 
@@ -15,29 +16,38 @@ async function getData(id: Number) {
 
 export default async function Home() {
   let pokeArray = [];
-  for(let id = 1; id < 13; id++){
+  for(let id = 1; id <= 25; id++){
     let pokeAPI = await getData(id);
     pokeArray.push(pokeAPI)
   }
   
 
   return (
-    <main className="flex min-h-screen flex-row items-center justify-between wrap">
-      <ul className="flex flex-row flex-wrap items-center justify-center w-100% h-100% gap-px">
+    <main className="flex min-h-screen flex-col items-center justify-between wrap">
+      <header className="flex flex-row items-center justify-between w-full h-8 px-10 mb-10">
+        <h1>
+          Pokedex
+        </h1>
+        <input type="text" />
+      </header>
+      <ul className="flex flex-row flex-wrap items-center justify-center w-100% h-100% gap-1.5 mb-5">
         {pokeArray.map((pokemon: any) => {
-          return <li className="flex flex-col w-1/6 h-500 border border-zinc-900 rounded-md">
+          return <li className="flex flex-col w-1/6 h-500 border border-zinc-500 rounded-md pb-3 bg-white">
                   <div className="flex flex-col items-center justify-between w-500 h-200 gap-25">
-                  <Image src={pokemon.sprites.front_default} width={150} height={50} alt="pokemon sprite"/>
-                  <Link href={`/pokemon`}>{pokemon.name}</Link>
-                  <div className="flex flex-row items-center justify-center gap-4 w-full">
-                    {pokemon.types.map((object: any)=> {
-                      return <span className={object.type.name}>{object.type.name}</span>
-                            })
-                    }
-                  </div> 
+                    <span>{'#' + pokemon.id}</span>
+                    <Image src={pokemon.sprites.front_default} width={150} height={50} alt="pokemon sprite"/>
+                    <Link href={`/pokemon`}>{pokemon.name.toUpperCase()}</Link>
+                    <div className="flex flex-row items-center justify-center gap-4 w-full">
+                      {pokemon.types.map((object: any)=> {
+                        return <span className={`type `+ object.type.name}>{object.type.name.toUpperCase()}</span>
+                              })
+                      }
+                    </div>
                   </div>
                 </li>
-        })}
+            }
+          )
+        }
       </ul> 
     </main>
   )
